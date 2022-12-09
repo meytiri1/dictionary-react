0d6c7ccac62daf7a54968c9aa6bf4db3880aed4c
Michelle K <109901620+meytiri1@users.noreply.github.com>
2022-12-09 15:14:16 +0100

    Built the basic search engine inside Dictionary componen

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary">"Hello world"</div>;
+  const [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for definition of ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <p>{keyword}</p>
+    </div>
+  );
 }
