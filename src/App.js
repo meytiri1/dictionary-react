46306fc4c3fdee30085d53bb200c435a007a6c4e
Michelle K <109901620+meytiri1@users.noreply.github.com>
2022-12-09 13:19:38 +0100

    Set-up dictionary app project & added logo to header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://bright-centaur-7034e8.netlify.app/"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer noopener"
         >
-          Learn React
+          <img src={logo} className="App-logo" alt="logo" />
         </a>
       </header>
     </div>
