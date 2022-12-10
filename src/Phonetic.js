8773a863f77242236db8f710ee92e7061b79ea1e
Michelle K <109901620+meytiri1@users.noreply.github.com>
2022-12-10 20:11:54 +0100

    Added sound button for phonetics with Howl package

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,44 @@
 import React from "react";
+import { Howl } from "howler";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer noopener">
-        Listen
-      </a>
-      <br />
-      {props.phonetic.text}
-    </div>
-  );
+
+  function soundPlay() {
+    const sound = new Howl({
+      src: props.phonetic.audio,
+      html5: true,
+      autoplay: false,
+      loop: false,
+      volume: 1,
+    });
+    sound.play();
+  }
+
+  if (props.phonetic.audio && props.phonetic.text) {
+    return (
+      <div className="Phonetic">
+        <button onClick={soundPlay}>Listen</button>
+        <span>{props.phonetic.text}</span>
+      </div>
+    );
+  } else {
+    if (props.phonetic.text) {
+      return (
+        <div className="Phonetic">
+          <span>{props.phonetic.text}</span>
+        </div>
+      );
+    } else {
+      if (props.phonetic.audio) {
+        return (
+          <div className="Phonetic">
+            <button onClick={soundPlay}>Listen</button>
+          </div>
+        );
+      } else {
+        return null;
+      }
+    }
+  }
 }
